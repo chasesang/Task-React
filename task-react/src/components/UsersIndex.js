3c9c1ba7573acec464d4c71d9bfdcfadf3517f6c
Chase Sang <chase.sang@gmail.com>
2017-06-01 11:11:34 -0700

    added searchbar

--- task-react/src/components/UsersIndex.js
+++ task-react/src/components/UsersIndex.js
@@ -7,22 +7,33 @@ class UsersIndex extends React.Component {
     super(props);
 
     this.state = {
-      users: []
+      users: [],
+      search:''
     };
   }
-
+  updateSearch(event) {
+      this.setState({search: event.target.value})
+    }
   componentDidMount() {
     getUsers()
       .then(users => {
         this.setState({
           users: users
+
         })
       }
     );
   }
 
+
+
   renderUsers() {
-    return this.state.users.map(user => {
+    let filteredUsers = this.state.users.filter(
+     (user) => {
+       return user.first_name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+     }
+   )
+    return filteredUsers.map(user => {
       return (
         <UserSummary onClick={this.props.onUserClick} key={user.id} {...user} />
       );
@@ -30,9 +41,14 @@ class UsersIndex extends React.Component {
   }
 
   render() {
+
     return (
       <div className="UsersIndex">
         <h2>Users</h2>
+        <input type="text"
+                value={this.state.search}
+                onChange={this.updateSearch.bind(this)}/>
+
         {this.renderUsers()}
       </div>
     );
